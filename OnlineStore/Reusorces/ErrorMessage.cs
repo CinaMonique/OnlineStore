724d7ef9e331217618f72ec8c837b3b0c42e351f
Monika <monika@example.com>
2018-07-05 14:00:40 +0200

    Extract error messages to separate class

---
+++ OnlineStore/Reusorces/ErrorMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace OnlineStore.Reusorces
+{
+    public static class ErrorMessage
+    {
+        public const string CategoryDoesNotExist = "Choosen category does not exist";
+
+        public const string SizeIdDoesNotExist = "Choosen size id does not exist";
+
+        public const string SizeDoesNotExist = "Choosen size does not exist";
+    }
+}
