8c082e1ba6a867b3439678d99a5ee317ce9589e0
Monika <monika@example.com>
2018-09-06 21:00:54 +0200

    Fix typo

--- OnlineStore/Reusorces/ErrorMessage.cs
+++ OnlineStore/Reusorces/ErrorMessage.cs
@@ -11,26 +11,26 @@ namespace OnlineStore.Reusorces
     {
         public const string CategoryIdNotSpecified = "Category id is not specified";
 
-        public const string CategoryDoesNotExist = "Choosen category does not exist";
+        public const string CategoryDoesNotExist = "Chosen category does not exist";
 
         public const string NoCategoryParameterProvided = "No category id parameter provided";
 
         public const string SizeIdNotSpecified = "Size id is not specified";
 
-        public const string SizeDoesNotExist = "Choosen size does not exist";
+        public const string SizeDoesNotExist = "Chosen size does not exist";
 
         public const string ProductIdNotSpecified = "Product id is not specified";
 
-        public const string ProductDoesNotExist = "Choosen product does not exist";
+        public const string ProductDoesNotExist = "Chosen product does not exist";
 
         public const string UserIdNotSpecified = "User id is not specified";
 
-        public const string UserDoesNotExist = "Choosen user does not exist";
+        public const string UserDoesNotExist = "Chosen user does not exist";
 
-        public const string CartDoesNotExist = "Choosen cart does not exist";
+        public const string CartDoesNotExist = "Chosen cart does not exist";
 
         public const string CartItemIdNotSpecified = "Cart item id is not specified";
 
-        public const string CartItemDoesNotExist = "Choosen cart item does not exist";
+        public const string CartItemDoesNotExist = "Chosen cart item does not exist";
     }
 }
